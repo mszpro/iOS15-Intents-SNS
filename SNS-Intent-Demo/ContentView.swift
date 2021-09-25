ddecc35dba4666b1bdf60dfe70a1c87e87bbb7ab
Shunzhe <git@mszmagic.com>
2021-09-25 14:40:17 +0800

    Initial Commit

---
+++ SNS-Intent-Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SNS-Intent-Demo
+//
+//  Created by Shunzhe Ma on 2021/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SNS-Intent-Demo/SNS_Intent_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SNS_Intent_DemoApp.swift
+//  SNS-Intent-Demo
+//
+//  Created by Shunzhe Ma on 2021/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SNS_Intent_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
